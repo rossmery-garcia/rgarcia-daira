1660c3fa5575b5b9c37c955987b832f9fdd6a940
rossmery-garcia <garciarossmery@gmail.com>
2024-02-05 07:34:49 -0300

    1.5 h + Server setup + Added /health endpoint + Added healthCheck function: check the health of the server + .gitignore added - GET endpoint: get the history of the operations

--- server/main.go
+++ server/main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+type HealthCheckResponse struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	response := HealthCheckResponse{
+		Status:  http.StatusOK,
+		Message: "OK",
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
+
 func main() {
-	fmt.Println("Hello Go!")
+	const PORT string = "3000"
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/health", healthCheck)
+
+	fmt.Println("Server running on port:", PORT)
+	log.Fatal(http.ListenAndServe(":"+PORT, router))
 }
