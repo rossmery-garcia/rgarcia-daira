5254dad405e50c0ee5185edafd8dcc4a41ed6598
rossmery-garcia <garciarossmery@gmail.com>
2024-02-05 09:12:23 -0300

    1.25 h + GET endpoint: get the history of the operations - POST endpoint: create operation

--- server/go.mod
+++ server/go.mod
@@ -2,4 +2,7 @@ module server
 
 go 1.21.6
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 )
 
@@ -13,6 +15,26 @@ type HealthCheckResponse struct {
 	Status  int    `json:"status"`
 	Message string `json:"message"`
 }
+
+type Operation struct {
+	Id           string    `json:"id"`
+	LeftOperand  int       `json:"left_operand"`  //-- First operand
+	RightOperand int       `json:"right_operand"` //-- Second operand
+	Operator     Operator  `json:"operator"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
+
+type Operator int
+
+const (
+	ADD Operator = iota
+	SUBT
+	MULT
+	DIV
+)
+
+var operationHistory = make([]Operation, 0)
 
 func healthCheck(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -26,11 +48,30 @@ func healthCheck(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
+func getOperations(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(operationHistory)
+}
+
 func main() {
 	const PORT string = "3000"
 	router := mux.NewRouter().StrictSlash(true)
 
-	router.HandleFunc("/health", healthCheck)
+	now := time.Now().UTC()
+	operationHistory = append(operationHistory,
+		Operation{
+			Id:           uuid.New().String(),
+			LeftOperand:  22,
+			RightOperand: 2,
+			Operator:     ADD,
+			CreatedAt:    now,
+			UpdatedAt:    now,
+		})
+
+	router.HandleFunc("/health", healthCheck).Methods("GET")
+	router.HandleFunc("/history", getOperations).Methods("GET")
 
 	fmt.Println("Server running on port:", PORT)
 	log.Fatal(http.ListenAndServe(":"+PORT, router))
